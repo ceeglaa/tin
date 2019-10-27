aceab5ecc5a54d0ae44e7e52803d83d6636380bb
ceeglaa <ceeglaa@gmail.com>
2019-10-27 22:38:20 +0100

    test

--- teste.ps1
+++ teste.ps1
@@ -6,7 +6,7 @@ $badContent = Get-Content "testPass.txt"
 Write-Host $badContent
 $Base64Token = [System.Convert]::ToBase64String([char[]]$Token);
 $base64AuthInfo = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(("{0}:{1}" -f $Username,$Password)))
-Write-Host $base64AuthInfo
+Write-Host $env:TOKEN
 $pass = ConvertTo-SecureString -AsPlainText $Password -Force
 $Cred = New-Object System.Management.Automation.PSCredential -ArgumentList $Username,$pass
 
@@ -23,5 +23,5 @@ $newBody = @{
 
 $newToekUrl = "https://api.github.com/authorizations"
 $UriCommit = "https://api.github.com/zen"
-Invoke-WebRequest -Uri $newToekUrl -Body $newBody -Headers $Headers2 -UseBasicParsing
-Invoke-WebRequest -Uri $UriCommit -Method GET -UseBasicParsing
+#Invoke-WebRequest -Uri $newToekUrl -Body $newBody -Headers $Headers2 -UseBasicParsing
+#Invoke-WebRequest -Uri $UriCommit -Method GET -UseBasicParsing
