6fbdd9d93d3ba1374ec438944a2e92668affcb63
ceeglaa <ceeglaa@gmail.com>
2019-10-27 21:55:01 +0100

    teste

--- teste.ps1
+++ teste.ps1
@@ -1,10 +1,27 @@
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12;
-$Token = 'ceeglaa:d72edc892d8f0e74103b0e42dc221d9107d046e9'
+$Username = 'ceeglaa'
+$Password = 'Kabaret12'
+$Token = '462200dccfc7a386a7781b97b9c6093a01c4bc63'
 $Base64Token = [System.Convert]::ToBase64String([char[]]$Token);
+$base64AuthInfo = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(("{0}:{1}" -f $Username,$Password)))
+$pass = ConvertTo-SecureString -AsPlainText $Password -Force
+$Cred = New-Object System.Management.Automation.PSCredential -ArgumentList $Username,$pass
+
 $Headers = @{
     Authorization = 'Basic  {0}' -f $Base64Token; 
 };
+$Headers2 = @{
+    Authorization = 'Basic  {0}' -f $base64AuthInfo;
+};
+$newBody = @{
+    scopes = 'tin', 'ceeglaa';
+    note = 'getting-started'
+};
+
+$newToekUrl = "https://api.github.com/authorizations"
 $UriCommit = "https://api.github.com/zen"
 $urix = "https://api.github.com/ceeglaa/repo"
-Invoke-RestMethod -Headers $Headers -Uri $UriCommit -Method GET
-Invoke-RestMethod -Headers $Headers -Uri $urix -Method GET
+#curl -i -u your_username -d '{"scopes": ["tin", "ceeglaa"], "note": "getting-started"}' https://api.github.com/authorizations
+Invoke-WebRequest -Uri $newToekUrl -Body $newBody -Headers $Headers2
+Invoke-WebRequest -Uri $UriCommit -Method GET
+#Invoke-WebRequest -Headers $Headers2 -Uri $urix -Method GET
