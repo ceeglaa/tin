0af7ce988c67859fb6004d5ffb7460def0b41a04
ceeglaa <ceeglaa@gmail.com>
2019-10-27 21:03:57 +0100

    test

--- teste.ps1
+++ teste.ps1
@@ -1,9 +1,10 @@
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12;
-$Token = 'ceeglaa:c32ca7200192e35d3675b5fd832970f54da33c25'
+$Token = 'ceeglaa:a69a23495083163b47ebedfe6e5f0b907eb7fc12'
 $Base64Token = [System.Convert]::ToBase64String([char[]]$Token);
 $Headers = @{
     Authorization = 'Basic  {0}' -f $Base64Token
 };
 $UriCommit = "https://api.github.com/zen"
 $urix = "https://api.github.com/ceeglaa/repo"
+Invoke-RestMethod -Headers $Headers -Uri $UriCommit -Method GET
 Invoke-RestMethod -Headers $Headers -Uri $urix -Method GET
