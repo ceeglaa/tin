5ee31b9c22ff5bf7780ed749bd09cbc20f967b46
ceeglaa <ceeglaa@gmail.com>
2020-01-14 21:39:05 +0100

    delete cors port number

--- spring/demo/src/main/java/com/tindrink/demo/rest/DrinkRestController.java
+++ spring/demo/src/main/java/com/tindrink/demo/rest/DrinkRestController.java
@@ -1,6 +1,8 @@
 package com.tindrink.demo.rest;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,11 +11,19 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 import java.util.Map;
+
+import javax.servlet.http.HttpSession;
 
 import com.fasterxml.jackson.annotation.JsonView;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -25,7 +35,7 @@ import com.tindrink.demo.entity.Drink;
 import com.tindrink.demo.entity.Ingredient;
 import com.tindrink.demo.entity.Views;
 
-@CrossOrigin(origins = "http://localhost:3001", maxAge = 3600)
+@CrossOrigin
 @RestController
 @RequestMapping("/api")
 public class DrinkRestController {
@@ -47,14 +57,14 @@ public class DrinkRestController {
         return drinkDao.findAll();
     }
 
-    // @GetMapping("/drinks/{drinkId}")
-    // @JsonView(Views.DrinkDetails.class)
-    // public Drink getDrinkDetail(@PathVariable int drinkId) {
-    //     return Drink.getDrink(drinkId);
-    // }
+    @GetMapping("/drinks/{drinkId}")
+    @JsonView(Views.DrinkDetails.class)
+    public Drink getDrinkDetail(@PathVariable int drinkId) {
+        return drinkDao.findById(drinkId);
+    }
 
     @PostMapping("/drinks")
-    public Drink createDrink(@RequestBody Map<String, Object> body) {
+    public ResponseEntity<String> createDrink(@RequestBody Map<String, Object> body) {
 
         Map<String, String> drinkMap = (Map<String, String>) body.get("drink");
         Map<String, Integer> amountMap = (Map<String, Integer>) body.get("amount");
@@ -72,7 +82,7 @@ public class DrinkRestController {
                 amountDao.save(amo);
             }); 
             drinkDao.save(drink);
-            return drink;
+            return new ResponseEntity<>("Drink o nazwie " + drink.getName() + " został dodany", HttpStatus.CREATED);
         } catch (IOException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
@@ -82,40 +92,58 @@ public class DrinkRestController {
     }
 
     @PutMapping("/drinks/{drinkId}")
-    public String updateDrink(@PathVariable int drinkId, @RequestBody Map<String, Object> body) {
+    public ResponseEntity<String> updateDrink(@PathVariable int drinkId, @RequestBody Map<String, Object> body) {
 
         Map<String, String> drinkMap = (Map<String, String>) body.get("drink");
         Map<String, Integer> amountMap = (Map<String, Integer>) body.get("amount");
 
         ObjectMapper mapper = new ObjectMapper(); // jackson's objectmapper
         
-        try {
+        try{
             Drink drink = mapper.readValue(mapper.writeValueAsString(drinkMap), Drink.class);
             drink.setId(drinkId);
             drinkDao.save(drink);
-        } catch (IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
+            Drink dr = drinkDao.findById(drinkId);
+            amountDao.deleteOnDrinkId(drinkId);
+
+            amountMap.entrySet().forEach(entry -> {
+                Ingredient ing = ingredientDao.findById(Integer.parseInt(entry.getKey()));
+                Amount amo = new Amount(entry.getValue());
+                dr.addAmount(amo);
+                ing.addAmount(amo);
+                amountDao.save(amo);
+            }); 
+        return new ResponseEntity<String>("Drink został zmieniony pomyślnie", HttpStatus.OK);
+        } catch (Exception e) {
+            return null;
         }
-
-        Drink dr = drinkDao.findById(drinkId);
-        amountDao.deleteOnDrinkId(drinkId);
-
-        amountMap.entrySet().forEach(entry -> {
-            Ingredient ing = ingredientDao.findById(Integer.parseInt(entry.getKey()));
-            Amount amo = new Amount(entry.getValue());
-            dr.addAmount(amo);
-            ing.addAmount(amo);
-            amountDao.save(amo);
-        }); 
   
-        return"hahaha";
     }
 
+    @PostMapping(value = "/drinks/photo")
+    public ResponseEntity<String> updateLotImage(@RequestParam("files") MultipartFile[] files,
+            RedirectAttributes redirectAttributes, HttpSession session) throws IOException {
+
+                byte[] bytes = files[0].getBytes();
+                Path path = Paths.get("./src/main/webapp/WEB-INF/images/" + files[0].getOriginalFilename());
+                Files.write(path, bytes);
+
+                return new ResponseEntity<String>("Success", HttpStatus.OK);
+
+    }
 
     @DeleteMapping("/drinks/{drinkId}")
-    public String deleteDrink(@PathVariable int drinkId) {
-        drinkDao.deleteById(drinkId);
-        return "Drink was removed successfully";
+    public ResponseEntity<String> deleteDrink(@PathVariable int drinkId) {
+        try {
+            System.out.println("USUWAM DRINKA ");
+            amountDao.deleteOnDrinkId(drinkId);
+            System.out.println("USUNALEM POWIAZANIA");
+            drinkDao.deleteById(drinkId);
+            return new ResponseEntity<String>("Drink został usunięty", HttpStatus.OK);
+        } catch (Exception e) {
+            return null;
+        }
+        
+        
     }
 }
--- spring/demo/src/main/java/com/tindrink/demo/rest/IngredientRestController.java
+++ spring/demo/src/main/java/com/tindrink/demo/rest/IngredientRestController.java
@@ -25,7 +25,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin(origins = "http://localhost:3001", maxAge = 3600)
+@CrossOrigin
 @RestController
 @RequestMapping("/api")
 public class IngredientRestController {
